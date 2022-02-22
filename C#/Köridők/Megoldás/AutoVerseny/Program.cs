dabb6d4e22681abdbdba3daed246938708604534
Dávid Patrik Gávrás <91025080+dewyd@users.noreply.github.com>
2022-02-22 19:26:53 +0100

    Add files via upload

---
+++ C#/Köridők/Megoldás/AutoVerseny/Program.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace AutoVerseny
+{
+    internal class Program
+    {
+        static List<Versenyzo> Korok = new List<Versenyzo>();
+
+        static void Main(string[] args)
+        {
+            #region Feladatok
+            Feladat2();
+            Console.WriteLine("3. feladat: {0}", Korok.Count);
+            Console.WriteLine("4. feladat: {0} másodperc", Feladat4());
+            Console.WriteLine("5. feladat");
+            Console.WriteLine("Kérem egy versenyző nevét: ");
+            string Nev = Console.ReadLine();
+            Console.WriteLine("6. feladat: {0}", Feladat6(Nev));
+            #endregion
+
+            Console.ReadKey();
+        }
+
+        #region 2. Feladat
+        static void Feladat2()
+        {
+            FileStream fs = new FileStream("autoverseny.csv", FileMode.Open);
+            StreamReader sr = new StreamReader(fs);
+
+            string s = sr.ReadLine();
+            while (!sr.EndOfStream)
+            {
+                s = sr.ReadLine();
+                string[] p1 = s.Split(';');
+                string[] p2 = p1[4].Split(':');
+                Versenyzo v = new Versenyzo(p1[0], p1[1], Convert.ToInt32(p1[2]), p1[3], Convert.ToInt32(p2[0]), Convert.ToInt32(p2[1]), Convert.ToInt32(p2[2]), Convert.ToInt32(p1[5]));
+                Korok.Add(v);
+            }
+            fs.Close();
+            sr.Close();
+        }
+        #endregion
+
+        #region 4. Feladat
+        static int Feladat4()
+        {
+            int mp = 0;
+            foreach (Versenyzo v in Korok)
+            {
+                if (v.Nev == "Fürge Ferenc" && v.Palya == "Gran Prix Circuit" && v.Kor == 3) { mp += (v.Korido[0] * 60) * 60; mp += v.Korido[1] * 60; mp += v.Korido[2]; }
+            }
+            return mp;
+        }
+        #endregion
+
+        #region 6. Feladat
+        static string Feladat6(string Nev)
+        {
+            bool van = false;
+            string Kor = ""; int min = 99999;
+            foreach (Versenyzo v in Korok)
+            {
+                if (v.Nev == Nev)
+                {
+                    van = true;
+                    int mp = (v.Korido[0] * 60) * 60;
+                    mp += v.Korido[1] * 60;
+                    mp += v.Korido[2];
+                    if (mp < min) { min = mp; Kor = String.Format("{0}, {1}:{2}:{3}", v.Palya, v.Korido[0], v.Korido[1], v.Korido[2]); }
+                }
+            }
+            if (van) { return Kor; }
+            else { return "Nincs ilyen versenyző az állományban!"; }
+        }
+        #endregion
+    }
+}
---
+++ C#/Köridők/Megoldás/AutoVerseny/Versenyzo.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AutoVerseny
+{
+    internal class Versenyzo
+    {
+        public string Csapat { get; set; }
+        public string Nev { get; set; }
+        public int Eletkor { get; set; }
+        public string Palya { get; set; }
+        public int[] Korido { get; set; }
+        public int Kor { get; set; }
+
+        public Versenyzo(string Csapat, string Nev, int Eletkor, string Palya, int Korido1, int Korido2, int Korido3, int Kor)
+        {
+            this.Csapat = Csapat;
+            this.Nev = Nev;
+            this.Eletkor = Eletkor;
+            this.Palya = Palya;
+            this.Korido = new int[3];
+            this.Korido[0] = Korido1;
+            this.Korido[1] = Korido2;
+            this.Korido[2] = Korido3;
+            this.Kor = Kor;
+        }
+    }
+}
